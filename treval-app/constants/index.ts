3e45f0ca2a0fd1dd85c4c775e4564053ac40a722
raghvendra1891 <95579510+raghvendra1891@users.noreply.github.com>
2024-09-04 22:08:55 +0530

    Add files via upload

---
+++ treval-app/constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Real maps can be offline',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae molestias eligendi, nulla quasi harum inventore debitis fuga, unde mollitia placeat nobis eum assumenda error blanditiis et quis accusantium aliquam veritatis.',
+  },
+  {
+    title: 'Set an adventure schedule',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae molestias eligendi, nulla quasi harum inventore debitis fuga, unde mollitia placeat nobis eum assumenda error blanditiis et quis accusantium aliquam veritatis.',
+  },
+  {
+    title: 'Technology using augment reality',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae molestias eligendi, nulla quasi harum inventore debitis fuga, unde mollitia placeat nobis eum assumenda error blanditiis et quis accusantium aliquam veritatis.',
+  },
+  {
+    title: 'Many new locations every month',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae molestias eligendi, nulla quasi harum inventore debitis fuga, unde mollitia placeat nobis eum assumenda error blanditiis et quis accusantium aliquam veritatis',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Hilink',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
